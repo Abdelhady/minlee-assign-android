7b3993d9c19179ad1a3b6356ddd45c9dfcc2d170
Abdel Hady <obad85@gmail.com>
2020-05-11 23:05:50 +0200

    Defaulting the backend url config to match the emulator setup

--- app/src/main/java/com/example/minleeAssign/Constants.kt
+++ app/src/main/java/com/example/minleeAssign/Constants.kt
@@ -6,9 +6,9 @@ package com.example.minleeAssign
 object Constants {
 
     // Update the following Ip to refer to the backend machine
-    val baseUrl = "http://192.168.1.4:3000/"
+//    val baseUrl = "http://192.168.1.4:3000/"
 
     // Use the following for emulator setup
-//    val baseUrl = "http://10.0.2.2:3000/"
+    val baseUrl = "http://10.0.2.2:3000/"
 
 }
